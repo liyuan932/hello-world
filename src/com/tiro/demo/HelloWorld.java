f086758ffadb4c5cbe2e54a6ff6ff246f98f63cd
liyuan <liyuan932@gmail.com>
2016-05-02 11:27:34 +0800

    Initial commit

---
+++ src/com/tiro/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.tiro.demo;
+
+/**
+ * @author Administrator
+ * @date 2016-05-02
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
